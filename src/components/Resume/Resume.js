a401eb4edf11aea84aba13e9e3ce3655974567d4
albinaabykanova <abykanovaalbina@gmail.com>
2023-01-12 22:14:43 +0300

    chanched color

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/albina.cv.pdf";
+import pdf from "../../Assets/albina.cv.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
-
 
 function Resume() {
   const [width, setWidth] = useState(1200);
@@ -34,7 +32,9 @@ function Resume() {
         </Row>
 
         <Row className="resume">
-          
+          <Document file={pdf} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
